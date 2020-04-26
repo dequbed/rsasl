48cf6ee17ba661192fc996444feb03d0b383250e
Gregor Reitzenstein <me@dequbed.space>
2020-04-26 12:53:19 +0200

    Extract parts into their own files

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@ use gsasl_sys::*;
 use std::ptr;
 use std::ffi::CStr;
 
+mod buffer;
+mod session;
+
+use session::Session;
+
 pub struct SASL {
     ctx: *mut Gsasl,
 }
@@ -65,88 +70,6 @@ impl Drop for SASL {
     fn drop(&mut self) {
         unsafe {
             gsasl_done(self.ctx);
-        }
-    }
-}
-
-pub struct Session {
-    ptr: *mut Gsasl_session,
-}
-
-impl Session {
-    pub fn from_ptr(ptr: *mut Gsasl_session) -> Self {
-        Self { ptr }
-    }
-
-    pub fn set_property(&mut self, prop: Gsasl_property, data: &[u8]) {
-        let data_ptr = data.as_ptr() as *const libc::c_char;
-        let len = data.len() as size_t;
-        unsafe {
-            gsasl_property_set_raw(self.ptr, prop, data_ptr, len);
-        }
-    }
-
-    pub fn step(&mut self, input: &[u8]) -> Result<SaslBuffer, libc::c_int> {
-
-        // rustc can't prove this will never be read so we need to initialize it to a (bogus)
-        // value.
-        let mut output: *mut libc::c_char = ptr::null_mut();
-        let mut output_len: size_t = 0;
-
-        let res;
-
-        unsafe {
-            res = gsasl_step(self.ptr, 
-                input.as_ptr() as *const libc::c_char, 
-                input.len() as size_t, 
-                &mut output as *mut *mut libc::c_char, 
-                &mut output_len as *mut size_t
-            );
-        }
-
-        if res != (Gsasl_rc_GSASL_OK as libc::c_int) {
-            Err(res)
-        } else {
-            Ok(SaslBuffer::from_parts(output, output_len as usize))
-        }
-
-    }
-}
-
-impl Drop for Session {
-    fn drop(&mut self) {
-        unsafe {
-            gsasl_finish(self.ptr);
-        }
-    }
-}
-
-pub struct SaslBuffer {
-    ptr: *mut u8,
-    len: usize,
-}
-
-impl SaslBuffer {
-    pub fn from_parts(ptr: *mut libc::c_char, len: usize) -> Self {
-        let ptr = ptr as *mut u8;
-        Self { ptr, len }
-    }
-}
-
-impl Drop for SaslBuffer {
-    fn drop(&mut self) {
-        unsafe {
-            gsasl_free(self.ptr as *mut libc::c_void);
-        }
-    }
-}
-
-impl std::ops::Deref for SaslBuffer {
-    type Target = [u8];
-
-    fn deref(&self) -> &Self::Target {
-        unsafe {
-            std::slice::from_raw_parts(self.ptr, self.len)
         }
     }
 }
