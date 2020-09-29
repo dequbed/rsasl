48dbc511d2230b83649d3eb3c7d3026a8df0e197
Nadja Reitzenstein <me@dequbed.space>
2020-09-29 23:18:22 +0200

    Enables building of gsasl-sys on docs.rs

--- gsasl-sys/src/lib.rs
+++ gsasl-sys/src/lib.rs
@@ -2,4 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+#[cfg(not(docsrs))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(docsrs)]
+include!(concat!("stale_bindings.rs"));
