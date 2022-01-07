be6895779ca45035c71b71ccd3585c4b4a24aac5
Nadja Reitzenstein <me@dequbed.space>
2022-01-07 21:59:42 +0100

    cleanup

--- src/mechanisms/anonymous/server.rs
+++ src/mechanisms/anonymous/server.rs
@@ -2,9 +2,11 @@ use std::ptr::NonNull;
 use ::libc;
 use libc::size_t;
 use crate::{Shared, SessionData};
+use crate::consts::{ANONYMOUS_TOKEN, AnonymousToken, GSASL_AUTHENTICATION_ERROR};
 use crate::gsasl::callback::gsasl_callback;
 use crate::gsasl::consts::{GSASL_ANONYMOUS_TOKEN, GSASL_MECHANISM_PARSE_ERROR, GSASL_NEEDS_MORE, GSASL_OK, GSASL_VALIDATE_ANONYMOUS};
 use crate::gsasl::property::gsasl_property_set_raw;
+use crate::validate::Anonymous;
 
 /* anonymous.h --- Prototypes for ANONYMOUS mechanism as defined in RFC 2245.
  * Copyright (C) 2002-2021 Simon Josefsson
@@ -56,20 +58,26 @@ pub unsafe fn _gsasl_anonymous_server_step(sctx: &mut SessionData,
                                            output_len: *mut size_t,
 ) -> libc::c_int
 {
-    let mut rc: libc::c_int = 0;
     *output = 0 as *mut libc::c_char;
     *output_len = 0 as libc::c_int as size_t;
     if input.is_none() { return GSASL_NEEDS_MORE as libc::c_int; }
     let input = input.unwrap();
-    /* token       = 1*255TCHAR
-     The <token> production is restricted to 255 UTF-8 encoded Unicode
-     characters.   As the encoding of a characters uses a sequence of 1
-     to 4 octets, a token may be long as 1020 octets. */
-    if input.len() == 0 || input.len() > 1020 {
-        return GSASL_MECHANISM_PARSE_ERROR as libc::c_int;
+    if let Ok(input) = std::str::from_utf8(input) {
+        /* token       = 1*255TCHAR
+         The <token> production is restricted to 255 UTF-8 encoded Unicode
+         characters.   As the encoding of a characters uses a sequence of 1
+         to 4 octets, a token may be long as 1020 octets. */
+        if input.len() == 0 || input.len() > 255 {
+            return GSASL_MECHANISM_PARSE_ERROR as libc::c_int;
+        }
+        sctx.set_property::<AnonymousToken>(Box::new(input.to_string()));
+
+        if let Err(_) = sctx.validate::<Anonymous>() {
+            GSASL_AUTHENTICATION_ERROR as libc::c_int
+        } else {
+            GSASL_OK as libc::c_int
+        }
+    } else {
+        GSASL_MECHANISM_PARSE_ERROR as libc::c_int
     }
-    /* FIXME: Validate that input is UTF-8. */
-    rc = gsasl_property_set_raw(sctx, GSASL_ANONYMOUS_TOKEN, input.as_ptr().cast(), input.len());
-    if rc != GSASL_OK as libc::c_int { return rc; }
-    return gsasl_callback(0 as *mut Shared, sctx, GSASL_VALIDATE_ANONYMOUS);
 }
