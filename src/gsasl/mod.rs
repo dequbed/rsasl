df90a25ce74c0356b0b59d56373b56d379ac9fe0
Nadja Reitzenstein <me@dequbed.space>
2022-01-07 12:39:53 +0100

    Remove code already implemented in Rust

--- src/gsasl/mod.rs
+++ src/gsasl/mod.rs
@@ -20,19 +20,12 @@ pub mod gl {
 pub mod base64;
 pub mod callback;
 pub mod crypto;
-pub mod done;
 pub mod error;
 pub mod free;
 pub mod init;
-pub mod mechname;
 pub mod mechtools;
 pub mod property;
 pub mod register;
 pub mod saslprep;
-pub mod suggest;
-pub mod supportp;
-pub mod xcode;
-pub mod xfinish;
-pub mod xstep;
 pub mod gc;
 
