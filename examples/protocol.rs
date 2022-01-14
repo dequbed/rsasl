2a8339ffc2bae7936181750d3b70eba3be17fc4c
Nadja Reitzenstein <me@dequbed.space>
2022-01-14 17:58:34 +0100

    Make suggestions work properly

--- examples/protocol.rs
+++ examples/protocol.rs
@@ -1,26 +1,23 @@
 use rsasl::{SASL};
 use rsasl::error::SASLError;
+use rsasl::mechname::Mechname;
 use rsasl::session::Session;
 
-struct ProtocolHandler {
-    sasl_handler: SASL,
-    authentication: Option<Session>,
-}
-
-impl ProtocolHandler {
-    fn handle_auth(&mut self, mechs: &[&str]) -> Result<(), SASLError> {
-        let mech = self.sasl_handler.client_start_suggested(mechs);
-        if let Some(mech) = mech {
-        }
-        todo!()
-    }
-}
-
 fn main() {
-    let provider = SASL::new();
+    let sasl = SASL::new();
 
-    let handler = ProtocolHandler {
-        sasl_handler: provider,
-        authentication: None,
-    };
+    let a = "LOGIN PLAIN GSSAPI".split_whitespace()
+        .map(|s| Mechname::new(s.as_bytes()))
+        .filter_map(|s| s.ok());
+    let presented = &[
+        Mechname::const_new_unchecked(b"LOGIN"),
+        Mechname::const_new_unchecked(b"PLAIN"),
+        Mechname::const_new_unchecked(b"GSSAPI"),
+        Mechname::const_new_unchecked(b"SCRAM-SHA-1"),
+        Mechname::const_new_unchecked(b"SCRAM-SHA-256"),
+    ];
+
+    let suggested = sasl.client_start_suggested(presented.iter().map(|m| *m)).unwrap();
+    println!("Suggested: {}", suggested.get_mechanism());
+    assert_eq!(suggested.get_mechanism().as_str(), "SCRAM-SHA-256");
 }
