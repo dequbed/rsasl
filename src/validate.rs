67c89f1bba66921e4fe91100027ebe35a5e44ce8
Nadja Reitzenstein <me@dequbed.space>
2022-07-13 22:30:51 +0200

    Add do action request as alt to satisfy request

--- src/validate.rs
+++ src/validate.rs
@@ -1,3 +1,4 @@
+use thiserror::Error;
 use crate::property::Property;
 use crate::typed::{tags, Erased, TaggedOption};
 use std::any::TypeId;
@@ -25,21 +26,49 @@ impl<'a> Validate<'a> {
     ///
     /// The final `outcome` value of a [`Validation`] depends on the specific mechanism that was
     /// used, but will usually be a `bool` or `Result` type.
-    pub fn finalize<T: Validation>(&mut self, outcome: T::Value) -> Result<(), ()> {
+    pub fn finalize<T: Validation>(&mut self, outcome: T::Value) -> Result<&mut Self, ()> {
         if let Some(result @ TaggedOption(Option::None)) = self.0.downcast_mut::<T>() {
             *result = TaggedOption(Some(outcome));
-            Ok(())
-        } else {
             Err(())
+        } else {
+            Ok(self)
+        }
+    }
+
+    pub fn with<T, F, E>(&mut self, f: F) -> Result<&mut Self, ValidationError>
+    where
+        T: Validation,
+        F: FnOnce() -> Result<T::Value, E>,
+        E: std::error::Error + Send + Sync + 'static
+    {
+        if let Some(result @ TaggedOption(Option::None)) = self.0.downcast_mut::<T>() {
+            match f() {
+                Ok(outcome) => {
+                    *result = TaggedOption(Some(outcome));
+                    Ok(self)
+                }
+                Err(error) => Err(ValidationError::Boxed(Box::new(error).into())),
+            }
+        } else {
+            Ok(self)
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum ValidationError {
+    #[error("validation callback is not implemented for this validation type")]
     NoValidation,
-    BadAuthentication,
-    BadAuthorization,
-    #[doc(hidden)]
-    EarlyReturn(PhantomData<()>),
+    #[error(transparent)]
+    Boxed(Box<dyn std::error::Error + Send + Sync>),
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+// Validation really is for the protocol implementation. So it should if anything return a type
+// *requested by the protocol implementation*, to allow more complex error types in the protocol
+// (think HTTP 401 vs 403 [auth failed, auth successful but with invalid authzid])
+pub enum ValidationOutcome {
+    Successful,
+    AuthenticationFailed,
+    AuthorizationFailed,
 }
