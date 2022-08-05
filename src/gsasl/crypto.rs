04774b0e40007fbd517cbdf775f098fe97cc7b47
Nadja Reitzenstein <me@dequbed.space>
2022-08-05 13:19:42 +0200

    remove all remaining code from gsasl

--- src/gsasl/crypto.rs
+++
@@ -1,6 +0,0 @@
-use crate::gsasl::gl::gc_gnulib::gc_nonce;
-use libc::size_t;
-
-pub unsafe fn gsasl_nonce(mut data: *mut libc::c_char, mut datalen: size_t) -> libc::c_int {
-    return gc_nonce(data, datalen) as libc::c_int;
-}
