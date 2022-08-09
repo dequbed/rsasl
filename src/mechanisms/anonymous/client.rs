265b367535a68dcb33a0f3a061e3966f4db2bf6c
Nadja Reitzenstein <me@dequbed.space>
2022-08-10 00:24:54 +0200

    ANONYMOUS tests

--- src/mechanisms/anonymous/client.rs
+++ src/mechanisms/anonymous/client.rs
@@ -21,3 +21,59 @@ impl Authentication for Anonymous {
         Ok((State::Finished, Some(len)))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+    use crate::callback::{Context, Request, SessionCallback, SessionData};
+    use crate::error::SessionError;
+    use crate::mechanisms::anonymous::AnonymousToken;
+    use crate::test;
+
+    struct C<'a> {
+        token: Option<&'a str>,
+    }
+    impl SessionCallback for C<'_> {
+        fn callback(
+            &self,
+            _session_data: &SessionData,
+            context: &Context,
+            request: &mut Request,
+        ) -> Result<(), SessionError> {
+            if let Some(token) = self.token {
+                request.satisfy::<AnonymousToken>(token)?;
+            }
+
+            Ok(())
+        }
+    }
+
+    fn test_token(token: Option<&'static str>, output: &[u8]) {
+        let config = test::server_config(C { token });
+        let mut session = test::client_session(config, &super::super::mechinfo::ANONYMOUS);
+        let mut out = Cursor::new(Vec::new());
+
+        let (state, written) = session.step(None, &mut out).unwrap();
+
+        let data = out.into_inner();
+        assert_eq!(written, Some(data.len()));
+        assert_eq!(output, &data[..]);
+        assert!(state.is_finished());
+    }
+
+    #[test]
+    fn test_with_token() {
+        let tokens = [
+            "thisisatoken",
+            "", // empty string should not error.
+        ];
+        for token in tokens {
+            test_token(Some(token), token.as_bytes());
+        }
+    }
+
+    #[test]
+    fn test_no_token() {
+        test_token(None, b"");
+    }
+}
